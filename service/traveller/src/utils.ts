ee27f3c67d3d1a500a7e00529903f119ec4e3333
Nat Weerawn <nat.wrw@gmail.com>
2022-06-25 11:23:17 +0700

    Add cli.ts

--- service/traveller/src/utils.ts
+++ service/traveller/src/utils.ts
@@ -1,5 +1,6 @@
 export const providers = {
-    1: '',
+    // 1: 'http://localhost:8547',
+    1: 'https://eth-mainnet.alchemyapi.io/v2/dTl4J-LTU9rPAo3Jcwl6gOm4Wy5wCsKZ',
     4: 'https://rinkeby.infura.io/v3/0b7da908f9f9495eb0f8de905c4fca66',
     100: '',
     10: '',
