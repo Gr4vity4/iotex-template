4d6c9c6c5e3a34fdc808ab846ca02b4c2b29b165
Nat W <nat.wrw@gmail.com>
2022-10-11 12:28:39 +0700

    Add hono

---
+++ hono-boilerplate/src/index.ts
@@ -0,0 +1,55 @@
+import { JsonRpcProvider } from '@ethersproject/providers'
+import { BigNumber, utils } from 'ethers'
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { poweredBy } from 'hono/powered-by'
+import { prettyJSON } from 'hono/pretty-json'
+import { getContentFromKVAsset } from 'hono/utils/cloudflare'
+import { DustBoyNFT, DustBoyNFT__factory } from '../typechain'
+import { providers } from './utils'
+const app = new Hono()
+
+app.use('*', poweredBy())
+app.use('*', prettyJSON())
+app.use('*', cors())
+
+const network = 4
+
+app.get('/hello', async (c) => {
+  const buffer = await getContentFromKVAsset(`hello.txt`)
+  const content = new TextDecoder().decode(buffer)
+  return c.json({ yay: content })
+})
+
+app.post('/auth', async (c) => {
+  return c.text('ok', 200)
+})
+
+app.post('/auth', async (c) => {
+  return c.text('ok', 200)
+})
+
+app.get('/', async (c) => {
+  const provider = new JsonRpcProvider({
+    url: providers[network],
+    skipFetchSetup: true,
+  })
+  const contract: DustBoyNFT = DustBoyNFT__factory.connect('0xCc9260CA4f41D04db0cfd9529a406607E9988EC5', provider)
+  console.log(await contract.MAX_SUPPLY())
+  return c.json({ hello: 'world yay 3' })!
+})
+
+app.get('/balance/jfin/:id', async (c) => {
+  const provider = new JsonRpcProvider({
+    url: providers[3501],
+    skipFetchSetup: true,
+  })
+
+  const balance: BigNumber = await provider.getBalance(c.req.param('id'))
+  const out = utils.formatEther(balance)
+  const outFloat = parseFloat(out)
+
+  return c.text(outFloat.toFixed(2))
+})
+
+app.fire()
---
+++ hono-boilerplate/src/utils.ts
@@ -0,0 +1,7 @@
+export const providers = {
+  1: '',
+  4: 'https://rinkeby.infura.io/v3/0b7da908f9f9495eb0f8de905c4fca66',
+  100: '',
+  10: '',
+  3501: 'https://rpc.jfinchain.com'
+}
